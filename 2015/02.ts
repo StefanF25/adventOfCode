960712779f2e12f81a8e3b46640f5dea5ed096ed
StefanF25 <fuchs912@gmail.com>
2025-10-31 20:14:34 +0100

    - solved part 2 of day 2

--- 2015/02.ts
+++ 2015/02.ts
@@ -1,8 +1,9 @@
-import { readFileSync } from 'fs'
+import {readFileSync} from 'fs'
 
 const input: string = readFileSync('./inputs/02.txt', 'utf-8')
 
-const getWrapping: (length: number, width: number, height: number) => number = (length: number, width: number, height: number): number => {
+const getWrapping = (dimensions: number[]): number => {
+    const [length, width, height] = dimensions
     const sides: number[] = [length * width, width * height, height * length];
     const extra: number = Math.min(...sides)
 
@@ -17,13 +18,30 @@ const getWrapping: (length: number, width: number, height: number) => number = (
     return wrapping
 }
 
-let total: number = 0
+const getRibbon = (dimensions: number[]): number => {
+    dimensions.sort((a, b) => a - b)
+    const [min, min2, min3] = dimensions
+    return (min + min2) * 2 + (min * min2 * min3)
+}
+
+// Examples
+console.log('Example 1 (2x3x4)')
+console.log(getWrapping([2, 3, 4]) + ' (expected 58)')
+console.log(getRibbon([2, 3, 4]) + ' (expected 34)')
+console.log('Example 2 (1x1x10)')
+console.log(getWrapping([1, 1, 10]) + ' (expected 43)')
+console.log(getRibbon([1, 1, 10]) + ' (expected 14)')
+
+let totalWrapping: number = 0
+let totalRibbon: number = 0
 
 for (const line of input.split("\n")) {
     if (line.length > 0) {
-        const [l, w, h] = line.split("x")
-        total += getWrapping(+l, +w, +h)
+        const dimensions: number[] = line.split("x").map(dim => parseInt(dim))
+        totalWrapping += getWrapping(dimensions)
+        totalRibbon += getRibbon(dimensions)
     }
 }
 
-console.log(`The total wrapping needed is ${total} square feet`)
+console.log(`The total wrapping needed is ${totalWrapping} square feet`)
+console.log(`The total ribbon needed is ${totalRibbon} feet`)
