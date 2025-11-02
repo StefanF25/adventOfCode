7577938bc1635c2ff16a59bb672f785a2ce0b2b6
StefanF25 <fuchs912@gmail.com>
2025-11-02 16:19:00 +0100

    - solved part 2 of day 4

--- 2015/04.ts
+++ 2015/04.ts
@@ -4,11 +4,14 @@ const createMd5: (str: string) => string = (str: string): string => createHash('
 
 const createHashString: (secret: string, num: (number | string)) => string = (secret: string, num: number | string): string => createMd5(secret + num)
 
-const iterateNumbers = (secret: string): [number, string] => {
+const iterateNumbers = (secret: string, leadingZeros: number = 5): [number, string] => {
     let input = 0
     let output = ''
 
-    while (!output.startsWith('00000')) {
+    console.log(`Secret: ${secret}`)
+    console.log(`${leadingZeros} leading zeros`)
+
+    while (!output.startsWith('0'.repeat(leadingZeros))) {
         input++
         output = createHashString(secret, input)
     }
@@ -17,17 +20,21 @@ const iterateNumbers = (secret: string): [number, string] => {
 }
 
 // Examples
-console.log('Example 1 (\'abcdef\')')
+console.log('Example 1')
 const [inputE1, outputE1] = iterateNumbers('abcdef')
 console.log(outputE1)
 console.log(`Input number: ${inputE1} (expected 609043)`)
-console.log('Example 2 (\'abcdef\')')
+
+console.log('Example 2')
 const [inputE2, outputE2] = iterateNumbers('pqrstuv')
 console.log(outputE2)
 console.log(`Input number: ${inputE2} (expected 1048970)`)
 
 const secret = 'iwrupvqb'
 const [input, output] = iterateNumbers(secret)
-console.log('Secret: ' + secret)
 console.log(output)
 console.log('Input number: ' + input)
+
+const [input2, output2] = iterateNumbers(secret, 6)
+console.log(output2)
+console.log('Input number: ' + input2)
